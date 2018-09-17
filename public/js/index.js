c7f23d20379368e324349e3a8aedb4abf0a32379
GlendaSalas <glenda.salas.a@gmail.com>
2018-09-17 14:40:04 -0500

    adding css

--- public/js/index.js
+++ public/js/index.js
@@ -16,25 +16,30 @@ socket.on('newLocationMessage', function(message){
 });
 
 $('#message-form').on('submit', function(e){
+  var messageTextbox=$('[name=message]');
   socket.emit('createMessage',{
       from: 'Franke',
-      text: $('[name=message]').val()
+      text: messageTextbox.val()
     },function(){
-
+      messageTextbox.val("");
     })
   e.preventDefault();
 });
 var locationButton= $('#send');
+
 locationButton.on('click', function(e){
   if(!navigator.geolocation){
     return alert('GEOLOCATION NOT SUPPORTED BY UR BROWSER.')
   }
+  locationButton.attr('disabled','disabled').text('Sending location... ');
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage',{
       latitude:position.coords.latitude,
       longitude:position.coords.longitude
     });
   },function(error){
+    locationButton.removeAttr('disabled').text('Send location');
     alert('UNABLE TO FETCH LOCATION')
   });
   e.preventDefault();
