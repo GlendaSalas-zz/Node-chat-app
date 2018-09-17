fff41d0b5ac6d6faff8a536526d84826ce74ec61
GlendaSalas <glenda.salas.a@gmail.com>
2018-09-17 10:38:21 -0500

    edit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,10 @@
 var socket= io();
 socket.on('connect',function(){
   console.log('conected to server');
-  // socket.emit('createEmail',{
-  //   to:'glenda.salas.a@outlook.com',
-  //   text:'Hey! this is andrew'
-  // })
-  socket.emit('createMessage',{
-    from:'glenda.salas.a@outlook.com',
-    text:'Hey! this is andrew'
-  })
-  socket.emit('newMessage',{
-    from:'glenda.salas.a@outlook.com',
-    text:'Hey! this is andrew',
-    createAt:12341234
-  })
 });
 socket.on('disconnect',function(){
   console.log('Disconnected from server');
 });
-socket.on('newEmail',function(email){
-  console.log('New email',email);
+socket.on('newMessage',function(message){
+  console.log('New message',message);
 });
--- server/server.js
+++ server/server.js
@@ -14,26 +14,22 @@ var server=http.createServer(app);
 var io =socketIO(server);
 
 app.use(express.static(publicPath));// CONFIGURATION EXPRESS
+
 io.on('connection',(socket)=>{
   console.log('NEW USER CONNECTED');
-  socket.emit('newEmail', {
-    from:'glenda.salas.a@gmail.com',
-    text:'hey',
-    createAt:123
-  });
+
   socket.on('createMessage',(message)=>{
     console.log('Create message',message);
+    io.emit('newMessage',{ // EVERY SINGLE PERSON
+      from: message.from,
+      text:message.text,
+      createAt: new Date().getTime()
+    });
   });
-  socket.on('newMessage',(message)=>{
-    console.log('New message',message);
-  });
-  // socket.on('createEmail',(newEmail)=>{
-  //   console.log('Create email',newEmail);
-  // });
   socket.on('disconnect',()=>{
     console.log('User disconnect');
   });
-}); // registra un evento
+});
 server.listen(port, ()=>{
   console.log(`SERVER PORT ${port}`)
 });
