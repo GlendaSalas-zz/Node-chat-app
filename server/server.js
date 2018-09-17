661ea4f88853b43896e73079b7859d6bb2e8aaba
GlendaSalas <glenda.salas.a@gmail.com>
2018-09-17 11:05:56 -0500

    create message utils

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const publicPath=path.join(__dirname,'../public');
 const  express = require('express');
 const  socketIO = require('socket.io');
-
+const {generateMessage}= require('./utils/message.js');
 const port=process.env.PORT;
 
 
@@ -16,23 +16,18 @@ var io =socketIO(server);
 app.use(express.static(publicPath));// CONFIGURATION EXPRESS
 
 io.on('connection',(socket)=>{
-  console.log('NEW USER CONNECTED');
-  socket.emit('newMessage', {
-    from : 'Admin',
-    text: 'Welcome to the chat app'
-  });
-  socket.broadcast.emit('newMessage',{
-    from:'Admin',
-    text:'New user joined',
-    createAt:new Date().getTime()
-  });
+  console.log('New user connected');
+  socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
   socket.on('createMessage',(message)=>{
     console.log('Create message',message);
-    io.emit('newMessage',{ // EVERY SINGLE PERSON
-      from: message.from,
-      text:message.text,
-      createAt: new Date().getTime()
-    });
+    io.emit('newMessage',generateMessage(message.from, message.text));
+    // { // EVERY SINGLE PERSON
+    //   from: message.from,
+    //   text:message.text,
+    //   createAt: new Date().getTime()
+    // }
+    // );
 
     socket.broadcast.emit('newMessage',{
        from: message.from,
