265c49026c42cda016854cab136337cbe743f34a
GlendaSalas <glenda.salas.a@gmail.com>
2018-09-17 10:50:32 -0500

    new user, and alert other users

--- server/server.js
+++ server/server.js
@@ -17,13 +17,27 @@ app.use(express.static(publicPath));// CONFIGURATION EXPRESS
 
 io.on('connection',(socket)=>{
   console.log('NEW USER CONNECTED');
-
+  socket.emit('newMessage', {
+    from : 'Admin',
+    text: 'Welcome to the chat app'
+  });
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New user joined',
+    createAt:new Date().getTime()
+  });
   socket.on('createMessage',(message)=>{
     console.log('Create message',message);
     io.emit('newMessage',{ // EVERY SINGLE PERSON
       from: message.from,
       text:message.text,
       createAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{
+       from: message.from,
+       text:message.text,
+       createAt: new Date().getTime()
     });
   });
   socket.on('disconnect',()=>{
